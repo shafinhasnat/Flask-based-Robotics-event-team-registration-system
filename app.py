e598be2311c83b381b59eb07ce0d5f787bb1bdab
USER <shafinhasnat97@gmail.com>
2019-10-31 19:18:15 +0600

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_login import login_user,logout_user, login_required,current_user,Logi
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = 'mieisthebestmiedaytekhubmojahobe'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 ENV = 'prod'
 if ENV == 'dev':
 	app.debug = True
@@ -13,7 +13,7 @@ if ENV == 'dev':
 else:
 	app.debug=False
 	app.config = 'postgres://aazmqvtooyrcmu:56e1c61f87f6e6cbed71ed4c0413c4441cbcdf08e12b71a040286714a41ceda3@ec2-174-129-253-27.compute-1.amazonaws.com:5432/def8rb96r96eld'
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
